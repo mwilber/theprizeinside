fb349da7b6e4adab39c791c427110d8cf5c8b871
Matthew Wilber <mwilber@gmail.com>
2012-06-30 14:23:07 -0400

    add add to pageadd panel share post

--- js/script.js
+++ js/script.js
@@ -25,3 +25,69 @@ $(document).ready(function(){
 		}
 	});
 });
+
+function fbshare(){
+	WallPost(social['link'] , social['title'] , social['description'] , social['image'] , '');
+}
+
+function twshare(){
+	var twcontent = escape(social['title'])+" - "+escape(social['description'])+" "+escape(social['link']);
+	openpopup('http://twitter.com/home?status='+twcontent,'tweeters',550,450);
+}
+
+function gpshare(){
+	var url = "https://plus.google.com/share?url="+escape(social['link']);
+	openpopup(url,'gplus',550,450);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//	Utility Functions
+/////////////////////////////////////////////////////////////////////////////
+
+// If the browser has a console, write to it.
+function DebugOut(newline){
+	try{
+		if (typeof console == "object"){ 
+			console.log(newline);
+		}
+	}catch(err){
+		
+	}
+	
+}
+
+// Open a popup centered on the screen.
+function openpopup(url,name,width,height)
+{
+	// Set up the window attrubutes
+	var attributes = "toolbar=0,location=0,height=" + height;
+	attributes = attributes + ",width=" + width;
+	var leftprop, topprop, screenX, screenY, cursorX, cursorY, padAmt;
+	
+	// Get the clients screen size
+	if(navigator.appName == "Microsoft Internet Explorer") {
+		screenY = screen.height;
+		screenX = screen.width;
+	}else{
+		screenY = window.outerHeight;
+		screenX = window.outerWidth;
+	}
+	
+	// Set the x/y position relative to the center of the screen
+	leftvar = (screenX - width) / 2;
+	rightvar = (screenY - height) / 2;
+	if(navigator.appName == "Microsoft Internet Explorer") {
+		leftprop = leftvar;
+		topprop = rightvar;
+	}else {
+		leftprop = (leftvar - pageXOffset);
+		topprop = (rightvar - pageYOffset);
+	}
+	attributes = attributes + ",left=" + leftprop;
+	attributes = attributes + ",top=" + topprop;
+
+	// Open the window
+	window.open(url,name,attributes)
+}
+
--- reactor/application/views/panel/panel_index.php
+++ reactor/application/views/panel/panel_index.php
@@ -2,9 +2,9 @@
 <div class="flexslider">
   <h2><?=$restaurant->restaurantTitle?></h2>
   <div class="sharegroup">
-	<a id="facebook" href="#" onclick="return false;"><img src="images/btn_fb.png" alt="facebook"/></a>
-	<a id="googleplus" href="#" onclick="return false;"><img src="images/btn_gp.png" alt="google plus"/></a>
-	<a id="tweeters" href="#" onclick="return false;"><img src="images/btn_tw.png" alt="Tweeters"/></a>
+	<a class="facebook" href="#" onclick="fbshare(); return false;"><img src="images/btn_fb.png" alt="facebook"/></a>
+	<a class="googleplus" href="#" onclick="gpshare(); return false;"><img src="images/btn_gp.png" alt="google plus"/></a>
+	<a class="tweeters" href="#" onclick="twshare(); return false;"><img src="images/btn_tw.png" alt="Tweeters"/></a>
 	<!--<a id="info" href="#" onclick="return false;"><img src="images/btn_info.png" alt="info"/></a>-->
   </div>
   <div class="clearfix"></div>
