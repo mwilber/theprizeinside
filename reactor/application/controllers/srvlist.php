90e194e8e2ed23350f22ae1d2c597ff6a2245569
Matthew Wilber <mwilber@gmail.com>
2014-02-01 20:57:34 -0500

    - Add checkins to restaurant listing

--- reactor/application/controllers/srvlist.php
+++ reactor/application/controllers/srvlist.php
@@ -15,10 +15,18 @@ class SrvList extends CI_Controller {
 		
 		$this->load->model('restaurant_model');
 		$this->load->model('prize_model');
+		$this->load->model('checkin_model');
 		$data = $this->restaurant_model->Get();
 		
 		foreach ($data as $restaurant) {
 			$restaurant->prize = $this->prize_model->Get(array('restaurantId'=>$restaurant->restaurantId,'prizeActive'=>1));
+			$restaurant->checkins = array();
+			$idx = 0;
+			//print_r($restaurant->prize);
+			foreach ($restaurant->prize as $prize) {
+				$restaurant->checkins[$idx] = $this->checkin_model->Get(array('prizeId'=>$prize->prizeId, 'limit'=>10));
+				$idx++;
+			}
 		}
 		
 		header('Content-type: application/json');
