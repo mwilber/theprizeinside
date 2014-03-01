77182d4d309aa6f2ba862348e4ef3aad31169203
Matthew Wilber <mwilber@gmail.com>
2014-03-01 16:21:14 -0500

    - remove proxy from api path

--- public/js/index_desktop.js
+++ public/js/index_desktop.js
@@ -21,9 +21,9 @@ var LOCATION_CK_INTERVAL = 15000;
 var DISTANCE_CHANGE_REFRESH_THRESHOLD = 10;
 var CHECKIN_NOTIFICATION_THRESHOLD = 0.05;
 
-//var apipath = "http://theprizeinside.com";
+var apipath = "http://theprizeinside.com";
 //var apipath = "http://gibson.loc/theprizeinside";
-var apipath = "https://rippleapi.herokuapp.com/xhr_proxy?tinyhippos_apikey=ABC&tinyhippos_rurl=https%3A//theprizeinside.com";
+//var apipath = "https://rippleapi.herokuapp.com/xhr_proxy?tinyhippos_apikey=ABC&tinyhippos_rurl=https%3A//theprizeinside.com";
 
 var wallmap = null;
 var userLocation = new google.maps.LatLng(0,0);
