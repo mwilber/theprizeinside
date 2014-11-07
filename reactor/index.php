be7533143d2b8e538dec8690c1e6a02b2ee7c8ad
Matt Wilber <mwilber@gmail.com>
2014-11-06 20:12:22 -0500

    enable cross domain requests. turn off production error reporting.

--- reactor/index.php
+++ reactor/index.php
@@ -67,11 +67,8 @@ switch (ENVIRONMENT)
 
 	case 'testing':
 	case 'production':
-		//error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
-		//ini_set('display_errors', 0);
-		error_reporting(E_ALL);
-		ini_set('display_startup_errors', true);
-		ini_set('display_errors', true);
+		error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
+		ini_set('display_errors', 0);
 	break;
 
 	default:
